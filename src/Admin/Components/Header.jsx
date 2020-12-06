f3682f6e1541d36952c23d4622e51df8f5ac6851
jinnkeyxx <68957615+jinnkeyxx@users.noreply.github.com>
2020-12-06 11:22:18 +0700

    a
    a

--- src/Admin/Components/Header.jsx
+++ src/Admin/Components/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
 
-const HeaderComponent = () => {
+const HeaderComponent = (props) => {
     return(
-        <h1> Header </h1>
+        <div class="wrapper">
+            <div class="sidebar">
+                <h2>Sidebar</h2>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Profile</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">portfolio</a></li>
+                    <li><a href="/"><i class="fas fa-blog"></i>Blogs</a></li>
+                    <li><a href="/">Contact</a></li>
+                    <li><a href="/">Map</a></li>
+                </ul>
+                <div class="social_media">
+
+                </div>
+            </div>
+            <div class="main_content">
+                <div class="header">{props.title}</div>
+                <div class="info">
+                    {props.children}
+             </div>
+            </div>
+        </div>
 
     )
 }
--- src/Admin/Components/Layout.jsx
+++ src/Admin/Components/Layout.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import HeaderComponent from './Header'
 import FooterComponent from './Footer'
-
+import './css/layout.css'
 const LayoutComponent = () => {
     return(
         <>
             <HeaderComponent/>
                 <h1>layout</h1>
-
             <FooterComponent/>
         </>
     )
